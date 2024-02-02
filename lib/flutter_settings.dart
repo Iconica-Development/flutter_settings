e8645e9b860d58ed47342908e9144a11a5305dc7
Freek van de Ven <freek@freekvandeven.nl>
2024-02-02 10:52:12 +0100

    feat: add settings mainpage

--- lib/flutter_settings.dart
+++ lib/flutter_settings.dart
@@ -1,8 +1,4 @@
 ///
 library flutter_settings;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/ui/settings_page.dart';
--- lib/src/ui/settings_page.dart
+++ lib/src/ui/settings_page.dart
@@ -1,8 +1,74 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_profile/flutter_profile.dart';
 
 class SettingsPage extends StatelessWidget {
-  const SettingsPage({super.key});
+  const SettingsPage({
+    required this.user,
+    this.settingsTitle = 'Profile',
+    this.logoutText = 'Log out',
+    this.editProfileText = 'Edit profile',
+    this.editProfileIcon,
+    this.extraSettings = const [],
+    this.onProfileEdit,
+    this.onProfileView,
+    this.onLogout,
+    super.key,
+  });
+
+  final User user;
+
+  final String settingsTitle;
+
+  final String logoutText;
+
+  final Widget? editProfileIcon;
+  final String editProfileText;
+
+  final List<Widget> extraSettings;
+
+  final VoidCallback? onProfileEdit;
+  final VoidCallback? onProfileView;
+
+  final VoidCallback? onLogout;
 
   @override
-  Widget build(BuildContext context) => const Placeholder();
+  Widget build(BuildContext context) => Column(
+        children: [
+          Text(settingsTitle),
+          GestureDetector(
+            onTap: onProfileView,
+            child: Avatar(
+              user: user,
+            ),
+          ),
+          Text('${user.firstName ?? ''} ${user.lastName ?? ''}'),
+          const SizedBox(height: 20),
+          const Divider(),
+          GestureDetector(
+            onTap: onProfileEdit,
+            child: Row(
+              children: [
+                const SizedBox(width: 20),
+                editProfileIcon ?? const Icon(Icons.person_2_outlined),
+                Text(editProfileText),
+                const Spacer(),
+                const Icon(Icons.arrow_forward_ios),
+                const SizedBox(width: 20),
+              ],
+            ),
+          ),
+          const Divider(),
+          ...extraSettings,
+          const Spacer(),
+          if (onLogout != null) ...[
+            TextButton(
+              onPressed: onLogout,
+              child: Text(
+                logoutText,
+                style: const TextStyle(decoration: TextDecoration.underline),
+              ),
+            ),
+          ],
+        ],
+      );
 }
